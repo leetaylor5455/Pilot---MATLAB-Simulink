2336deb740289e6af825f7023f48772b5cfb87ca
freddie189 <118919399+freddie189@users.noreply.github.com>
2024-11-13 16:05:56 +0000

    updated

--- montecarlo3dof.m
+++ montecarlo3dof.m
@@ -1,14 +1,18 @@
-runs=30;
+runs=10;
 startingtime=0;
 f1 = figure;
-tiledlayout(1,2);
+tiledlayout(1,3);
 a= nexttile;
 b= nexttile;
+c=nexttile;
 for i=1:runs
     Gimbal_offset_X=randn*0.0174533*0.33;
     sim("threedof.slx");
     Positionvector = load("Position3dof.mat");
     Orientationvector= load("Orientation3dof.mat");
+    anglevector=load("gimbalangle.mat");
+    gimbalangle=anglevector.angle(2,1:329);
+    timeg=anglevector.angle(1,1:329);
     Position_X = Positionvector.position(2,:);
     Position_Z = Positionvector.position(3,:);
     Orientation= Orientationvector.orientation(2,:);
@@ -19,16 +23,23 @@ for i=1:runs
     axes(b)
     hold on
     plot(Position_X,Position_Z)
+    axes(c)
+    hold on
+    plot(timeg,gimbalangle)
     
    
 end
-
+axes(b)
 title(sprintf('%d Simulated Monte-Carlo Flight Trajectories',runs))
 xlabel('Crossrange (m)')
 ylabel('Altitude (m)')
 axis equal 
 axes(a)
 title('Orientation ')
+xlabel('Time(s)')
+ylabel('Orientation (radians)')
+axes(c)
+title('gimbal angle')
 xlabel('Time(s)')
 ylabel('Orientation (radians)')
 
